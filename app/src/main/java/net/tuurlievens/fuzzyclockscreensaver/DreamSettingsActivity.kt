a54bffb5be2cbfcc5358bab25b0ec349eabd299d
Tuur Lievens <tuur.lievens@hotmail.com>
2018-12-09 16:23:04 +0100

    Release v0.1

---
+++ app/src/main/java/net/tuurlievens/fuzzyclockscreensaver/DreamSettingsActivity.kt
@@ -0,0 +1,91 @@
+package net.tuurlievens.fuzzyclockscreensaver
+
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import android.preference.ListPreference
+import android.preference.Preference
+import android.preference.PreferenceActivity
+import android.preference.PreferenceFragment
+import android.preference.PreferenceManager
+import android.view.MenuItem
+import android.view.WindowManager
+
+
+class DreamSettingsActivity : PreferenceActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            window.statusBarColor = resources.getColor(android.R.color.black)
+        }
+
+        fragmentManager.beginTransaction().replace(android.R.id.content, AllPreferenceFragment()).commit()
+    }
+
+    class AllPreferenceFragment : PreferenceFragment() {
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            addPreferencesFromResource(R.xml.prefs)
+            setHasOptionsMenu(true)
+
+            // Bind the summaries of EditText/List/Dialog preferences
+            // to their values. When their values change, their summaries are
+            // updated to reflect the new value, per the Android Design
+            // guidelines.
+            bindPreferenceSummaryToValue(findPreference("maxTranslationDisplacement"))
+            bindPreferenceSummaryToValue(findPreference("updateSeconds"))
+            bindPreferenceSummaryToValue(findPreference("language"))
+        }
+
+        override fun onOptionsItemSelected(item: MenuItem): Boolean {
+            val id = item.itemId
+            if (id == android.R.id.home) {
+                startActivity(Intent(activity, DreamSettingsActivity::class.java))
+                return true
+            }
+            return super.onOptionsItemSelected(item)
+        }
+    }
+
+    companion object {
+
+        private val sBindPreferenceSummaryToValueListener = Preference.OnPreferenceChangeListener { preference, value ->
+            val stringValue = value.toString()
+
+            if (preference is ListPreference) {
+                // For list preferences, look up the correct display value in
+                // the preference's 'entries' list.
+                val index = preference.findIndexOfValue(stringValue)
+
+                // Set the summary to reflect the new value.
+                preference.setSummary(
+                    if (index >= 0)
+                        preference.entries[index]
+                    else
+                        null
+                )
+
+            } else {
+                // For all other preferences, set the summary to the value's
+                // simple string representation.
+                preference.summary = stringValue
+            }
+            true
+        }
+
+        private fun bindPreferenceSummaryToValue(preference: Preference) {
+            // Set the listener to watch for value changes.
+            preference.onPreferenceChangeListener = sBindPreferenceSummaryToValueListener
+
+            // Trigger the listener immediately with the preference's
+            // current value.
+            sBindPreferenceSummaryToValueListener.onPreferenceChange(
+                preference,
+                PreferenceManager.getDefaultSharedPreferences(preference.context).getString(preference.key, "")
+            )
+        }
+    }
+}
---
+++ app/src/main/java/net/tuurlievens/fuzzyclockscreensaver/FuzzyClockDream.kt
@@ -0,0 +1,105 @@
+package net.tuurlievens.fuzzyclockscreensaver
+
+import android.os.Build
+import android.os.Handler
+import android.preference.PreferenceManager
+import android.service.dreams.DreamService
+import android.view.View
+import android.widget.RelativeLayout
+import android.widget.TextView
+import java.util.*
+import android.util.DisplayMetrics
+
+
+class FuzzyClockDream : DreamService() {
+
+    private var timer = Timer("FuzzyClockTimer", true)
+    private lateinit var task: TimerTask
+    private var handler =  Handler()
+
+    private var maxTranslationDisplacement = 0.1
+    private var updateSeconds = 60.0
+    private var language = "default"
+
+    // LIFECYCLE
+
+    override fun onAttachedToWindow() {
+        super.onAttachedToWindow()
+
+        loadSettings()
+        createTask()
+
+        // Hide system UI
+        isFullscreen = true
+        window.decorView.apply {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                systemUiVisibility = View.SYSTEM_UI_FLAG_IMMERSIVE or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_FULLSCREEN
+            } else {
+                systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_FULLSCREEN
+            }
+        }
+
+        // Exit dream upon user touch
+        isInteractive = false
+        // Dim screen
+        isScreenBright = false
+        // Set the dream layout
+        setContentView(R.layout.dream)
+    }
+
+    override fun onDreamingStarted() {
+        // Show initial value of clock
+        task.run()
+        // Update clock every minute
+        timer.scheduleAtFixedRate(task, 0, (updateSeconds*1000).toLong())
+    }
+
+    override fun onDreamingStopped() {}
+
+    override fun onDetachedFromWindow() {
+        timer.cancel()
+    }
+
+    // HELPERS
+
+    private fun loadSettings() {
+        val prefs = PreferenceManager.getDefaultSharedPreferences(this)
+        maxTranslationDisplacement = prefs.getString("maxTranslationDisplacement","").toDouble()
+        updateSeconds = prefs.getString("updateSeconds","").toDouble()
+        language = prefs.getString("language","")
+    }
+
+    private fun createTask() {
+
+        task = object : TimerTask() {
+            override fun run() {
+
+                handler.post {
+                    // get the current timestamp
+                    val calendar = Calendar.getInstance()
+                    val hour = calendar.get(Calendar.HOUR_OF_DAY)
+                    val min = calendar.get(Calendar.MINUTE)
+
+                    // update clock
+                    val pickedLanguage = if (language == "default") Locale.getDefault().language else language
+                    findViewById<TextView>(R.id.clocktext).text = FuzzyTextGenerator.create(hour, min, pickedLanguage)
+
+                    // move clock around (randomly max 10% of total) against burn in
+                    val parent = findViewById<RelativeLayout>(R.id.clockparent)
+                    val display = DisplayMetrics()
+                    windowManager.defaultDisplay.getMetrics(display)
+
+                    parent.translationX = calcRandomTranslation(display.widthPixels * maxTranslationDisplacement)
+                    parent.translationY = calcRandomTranslation(display.heightPixels * maxTranslationDisplacement)
+                }
+            }
+        }
+
+    }
+
+    private fun calcRandomTranslation(max: Double) : Float {
+        val random = Random().nextFloat()
+        return Math.round( random*max*2 - max ).toFloat()
+    }
+
+}
