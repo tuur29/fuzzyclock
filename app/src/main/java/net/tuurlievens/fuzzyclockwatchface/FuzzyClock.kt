019fdda8bc7186b94140fb012ccd4a0edddfc6ee
Tuur Lievens <tuur.lievens@hotmail.com>
2018-12-10 23:39:11 +0100

    Release v0.1

---
+++ app/src/main/java/net/tuurlievens/fuzzyclockwatchface/FuzzyClock.kt
@@ -0,0 +1,316 @@
+package net.tuurlievens.fuzzyclockwatchface
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Rect
+import android.graphics.Typeface
+import android.os.Bundle
+import android.os.Handler
+import android.os.Message
+import android.support.v4.content.ContextCompat
+import android.support.wearable.watchface.CanvasWatchFaceService
+import android.support.wearable.watchface.WatchFaceService
+import android.support.wearable.watchface.WatchFaceStyle
+import android.text.DynamicLayout
+import android.text.Layout
+import android.text.TextPaint
+import android.view.Gravity
+import android.view.SurfaceHolder
+import android.view.WindowInsets
+import android.widget.Toast
+import net.tuurlievens.fuzzyclockscreensaver.FuzzyTextGenerator
+
+import java.lang.ref.WeakReference
+import java.util.Calendar
+import java.util.TimeZone
+
+/**
+ * Digital watch face with seconds. In ambient mode, the seconds aren't displayed. On devices with
+ * low-bit ambient mode, the text is drawn without anti-aliasing in ambient mode.
+ *
+ *
+ * Important Note: Because watch face apps do not have a default Activity in
+ * their project, you will need to set your Configurations to
+ * "Do not launch Activity" for both the Wear and/or Application modules. If you
+ * are unsure how to do this, please review the "Run Starter project" section
+ * in the Google Watch Face Code Lab:
+ * https://codelabs.developers.google.com/codelabs/watchface/index.html#0
+ */
+class FuzzyClock : CanvasWatchFaceService() {
+
+    companion object {
+        private val NORMAL_TYPEFACE = Typeface.create(Typeface.SANS_SERIF, Typeface.NORMAL)
+
+        /**
+         * Updates rate in milliseconds for interactive mode. We update once a second since seconds
+         * are displayed in interactive mode.
+         */
+        private const val INTERACTIVE_UPDATE_RATE_MS = 30*1000
+
+        /**
+         * Handler message id for updating the time periodically in interactive mode.
+         */
+        private const val MSG_UPDATE_TIME = 0
+    }
+
+    override fun onCreateEngine(): Engine {
+        return Engine()
+    }
+
+    private class EngineHandler(reference: FuzzyClock.Engine) : Handler() {
+        private val mWeakReference: WeakReference<FuzzyClock.Engine> = WeakReference(reference)
+
+        override fun handleMessage(msg: Message) {
+            val engine = mWeakReference.get()
+            if (engine != null) {
+                when (msg.what) {
+                    MSG_UPDATE_TIME -> engine.handleUpdateTimeMessage()
+                }
+            }
+        }
+    }
+
+    inner class Engine : CanvasWatchFaceService.Engine() {
+
+        private lateinit var mCalendar: Calendar
+
+        private var mRegisteredTimeZoneReceiver = false
+
+        private var mXOffset: Float = 0F
+        private var mYOffset: Float = 0F
+
+        private lateinit var mBackgroundPaint: Paint
+        private lateinit var mTextPaint: TextPaint
+
+        /**
+         * Whether the display supports fewer bits for each color in ambient mode. When true, we
+         * disable anti-aliasing in ambient mode.
+         */
+        private var mLowBitAmbient: Boolean = false
+        private var mBurnInProtection: Boolean = false
+        private var mAmbient: Boolean = false
+
+        private val mUpdateTimeHandler: Handler = EngineHandler(this)
+
+        private val mTimeZoneReceiver: BroadcastReceiver = object : BroadcastReceiver() {
+            override fun onReceive(context: Context, intent: Intent) {
+                mCalendar.timeZone = TimeZone.getDefault()
+                invalidate()
+            }
+        }
+
+        override fun onCreate(holder: SurfaceHolder) {
+            super.onCreate(holder)
+
+
+            setWatchFaceStyle(
+                WatchFaceStyle.Builder(this@FuzzyClock)
+                    .setAcceptsTapEvents(true)
+                    .setStatusBarGravity(Gravity.CENTER_HORIZONTAL or Gravity.TOP)
+//                    .setShowUnreadCountIndicator(true) // TODO: test this
+                    .build()
+            )
+
+            mCalendar = Calendar.getInstance()
+
+            val resources = this@FuzzyClock.resources
+            mYOffset = resources.getDimension(R.dimen.digital_y_offset)
+
+            // Initializes background.
+            mBackgroundPaint = Paint().apply {
+                color = ContextCompat.getColor(applicationContext, R.color.background)
+            }
+
+            // Initializes Watch Face.
+            mTextPaint = TextPaint().apply {
+                typeface = NORMAL_TYPEFACE
+                isAntiAlias = true
+                color = ContextCompat.getColor(applicationContext, R.color.digital_text)
+            }
+        }
+
+        override fun onDestroy() {
+            mUpdateTimeHandler.removeMessages(MSG_UPDATE_TIME)
+            super.onDestroy()
+        }
+
+        override fun onPropertiesChanged(properties: Bundle) {
+            super.onPropertiesChanged(properties)
+            mLowBitAmbient = properties.getBoolean(
+                WatchFaceService.PROPERTY_LOW_BIT_AMBIENT, false
+            )
+            mBurnInProtection = properties.getBoolean(
+                WatchFaceService.PROPERTY_BURN_IN_PROTECTION, false
+            )
+        }
+
+        override fun onTimeTick() {
+            super.onTimeTick()
+            invalidate()
+        }
+
+        override fun onAmbientModeChanged(inAmbientMode: Boolean) {
+            super.onAmbientModeChanged(inAmbientMode)
+            mAmbient = inAmbientMode
+
+            if (mLowBitAmbient) {
+                mTextPaint.isAntiAlias = !inAmbientMode
+            }
+
+            // Whether the timer should be running depends on whether we're visible (as well as
+            // whether we're in ambient mode), so we may need to start or stop the timer.
+            updateTimer()
+        }
+
+        /**
+         * Captures tap event (and tap type) and toggles the background color if the user finishes
+         * a tap.
+         */
+        override fun onTapCommand(tapType: Int, x: Int, y: Int, eventTime: Long) {
+            when (tapType) {
+                WatchFaceService.TAP_TYPE_TOUCH -> {
+                    // The user has started touching the screen.
+                }
+                WatchFaceService.TAP_TYPE_TOUCH_CANCEL -> {
+                    // The user has started a different gesture or otherwise cancelled the tap.
+                }
+                WatchFaceService.TAP_TYPE_TAP ->
+                    // The user has completed the tap gesture.
+                    // TODO: Add code to handle the tap gesture.
+                    Toast.makeText(applicationContext, R.string.message, Toast.LENGTH_SHORT)
+                        .show()
+            }
+            invalidate()
+        }
+
+        override fun onDraw(canvas: Canvas, bounds: Rect) {
+            // Draw the background.
+            if (mAmbient) {
+                canvas.drawColor(Color.BLACK)
+            } else {
+                canvas.drawRect(
+                    0f, 0f, bounds.width().toFloat(), bounds.height().toFloat(), mBackgroundPaint
+                )
+            }
+
+            // TODO: select correct locale
+            val text = FuzzyTextGenerator.create(mCalendar.get(Calendar.HOUR), mCalendar.get(Calendar.MINUTE), "")
+//            val xPos = (canvas.width / 2).toFloat()
+//            val yPos = ((canvas.height / 2) - ((mTextPaint.descent() + mTextPaint.ascent()) / 2))
+//            canvas.drawText(text, xPos, yPos, mTextPaint)
+
+            val layout = DynamicLayout(text, mTextPaint, bounds.width(), Layout.Alignment.ALIGN_CENTER, 1F, 1F, true)
+
+            canvas.save()
+
+            val textHeight = getTextHeight(text, mTextPaint)
+            val numberOfTextLines = layout.lineCount
+            val textXCoordinate = bounds.left.toFloat()
+            val textYCoordinate = bounds.exactCenterY() - numberOfTextLines * textHeight / 2
+            canvas.translate(textXCoordinate, textYCoordinate)
+
+            layout.draw(canvas)
+            canvas.restore()
+        }
+
+        private fun getTextHeight(text: String, paint: Paint): Float {
+            val rect = Rect()
+            paint.getTextBounds(text, 0, text.length, rect)
+            return rect.height().toFloat()
+        }
+
+        override fun onVisibilityChanged(visible: Boolean) {
+            super.onVisibilityChanged(visible)
+
+            if (visible) {
+                registerReceiver()
+
+                // Update time zone in case it changed while we weren't visible.
+                mCalendar.timeZone = TimeZone.getDefault()
+                invalidate()
+            } else {
+                unregisterReceiver()
+            }
+
+            // Whether the timer should be running depends on whether we're visible (as well as
+            // whether we're in ambient mode), so we may need to start or stop the timer.
+            updateTimer()
+        }
+
+        private fun registerReceiver() {
+            if (mRegisteredTimeZoneReceiver) {
+                return
+            }
+            mRegisteredTimeZoneReceiver = true
+            val filter = IntentFilter(Intent.ACTION_TIMEZONE_CHANGED)
+            this@FuzzyClock.registerReceiver(mTimeZoneReceiver, filter)
+        }
+
+        private fun unregisterReceiver() {
+            if (!mRegisteredTimeZoneReceiver) {
+                return
+            }
+            mRegisteredTimeZoneReceiver = false
+            this@FuzzyClock.unregisterReceiver(mTimeZoneReceiver)
+        }
+
+        override fun onApplyWindowInsets(insets: WindowInsets) {
+            super.onApplyWindowInsets(insets)
+
+            // Load resources that have alternate values for round watches.
+            val resources = this@FuzzyClock.resources
+            val isRound = insets.isRound
+            mXOffset = resources.getDimension(
+                if (isRound)
+                    R.dimen.digital_x_offset_round
+                else
+                    R.dimen.digital_x_offset
+            )
+
+            val textSize = resources.getDimension(
+                if (isRound)
+                    R.dimen.digital_text_size_round
+                else
+                    R.dimen.digital_text_size
+            )
+
+            mTextPaint.textSize = textSize
+        }
+
+        /**
+         * Starts the [.mUpdateTimeHandler] timer if it should be running and isn't currently
+         * or stops it if it shouldn't be running but currently is.
+         */
+        private fun updateTimer() {
+            mUpdateTimeHandler.removeMessages(MSG_UPDATE_TIME)
+            if (shouldTimerBeRunning()) {
+                mUpdateTimeHandler.sendEmptyMessage(MSG_UPDATE_TIME)
+            }
+        }
+
+        /**
+         * Returns whether the [.mUpdateTimeHandler] timer should be running. The timer should
+         * only run when we're visible and in interactive mode.
+         */
+        private fun shouldTimerBeRunning(): Boolean {
+            return isVisible && !isInAmbientMode
+        }
+
+        /**
+         * Handle updating the time periodically in interactive mode.
+         */
+        fun handleUpdateTimeMessage() {
+            invalidate()
+            if (shouldTimerBeRunning()) {
+                val timeMs = System.currentTimeMillis()
+                val delayMs = INTERACTIVE_UPDATE_RATE_MS - timeMs % INTERACTIVE_UPDATE_RATE_MS
+                mUpdateTimeHandler.sendEmptyMessageDelayed(MSG_UPDATE_TIME, delayMs)
+            }
+        }
+    }
+}
